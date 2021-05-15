9a42e3168ce15a949fd9481187744d98b6436688
kite <2257440643@qq.com>
2021-05-15 22:26:12 +0800

    完成商品功能

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-import TreeTable from 'vue-table-with-tree-grid'
+import TreeTable from 'vue-table-with-tree-grid';
+import VueQuillEditor from 'vue-quill-editor';
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 // 导入全局样式
@@ -22,7 +27,22 @@ axios.interceptors.request.use(config => {
 
 Vue.config.productionTip = false;
 
+// 注册主见
 Vue.component('tree-table', TreeTable);
+Vue.use(VueQuillEditor);
+
+// 全局时间过滤器
+Vue.filter('dateFormat', (date) =>{
+  const time = new Date(date);
+  const y = time.getFullYear();
+  const m = (time.getMonth() + 1).toString().padStart(2, '0');
+  const d = (time.getDate()).toString().padStart(2, '0');
+  const hh = (time.getHours()).toString().padStart(2, '0');
+  const mm = (time.getMinutes()).toString().padStart(2, '0');
+  const ss = (time.getSeconds()).toString().padStart(2, '0');
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
