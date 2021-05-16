e14e26a19e82d1edf825747d22370bb147f4dec0
kite <2257440643@qq.com>
2021-05-16 14:20:26 +0800

    订单功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -38,7 +38,10 @@ import {
   CheckboxGroup,
   Checkbox,
   Upload
-} from 'element-ui'
+} from 'element-ui';
+
+import Timeline from './timeline/index'
+import TimelineItem from './timeline-item/index'
 
 Vue.use(Button);
 Vue.use(Form);
@@ -76,7 +79,8 @@ Vue.use(Step);
 Vue.use(CheckboxGroup);
 Vue.use(Checkbox);
 Vue.use(Upload);
-
+Vue.use(Timeline);
+Vue.use(TimelineItem);
 
 Vue.prototype.$message = Message;
 Vue.prototype.$confirm = MessageBox.confirm;
